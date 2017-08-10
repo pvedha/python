06cece4dbcfc76c7f100aea3c9aad05f278607e9
Praveen Vedha <pvedha@cisco.com>
2017-08-10 12:21:02 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,19 @@
+print("what is PYTHON???")
+
+
+def add_no(a, b):
+    print(a+b)
+
+
+add_no(4,5)
+
+#for index in range(5):
+#    print("Printing Index alone {0}".format(index))
+#for index in range(5, 10):
+#   print("Printing Index from 5 {0}".format(index))
+
+for index in range(5, 20, 2):
+    #print("Printing Index with two step increment {0}".format(index))
+    if index > 10:
+        break
+
